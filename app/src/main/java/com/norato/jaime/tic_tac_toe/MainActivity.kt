7f245abfb6e8ef1b98599a3ee706b413e6f89d12
Jaime Norato <jn901113jn@gmail.com>
2018-03-03 19:10:03 -0500

    inicio proyecto tic tac toe

---
+++ app/src/main/java/com/norato/jaime/tic_tac_toe/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.norato.jaime.tic_tac_toe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
